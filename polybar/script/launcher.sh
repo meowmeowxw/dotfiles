5e8d46ab1f37274bdf72ff6e44d2ab83ee133b85
meowmeowxw <43200867+meowmeowxw@users.noreply.github.com>
2019-03-06 22:59:34 +0100

    sciprts

--- polybar/script/launcher.sh
+++ polybar/script/launcher.sh
@@ -2,4 +2,4 @@
 
 ## Created By Aditya Shakya
 
-rofi -modi run,drun -show drun -location 7 -xoffset 14 -yoffset -36 -line-padding 4 -columns 1 -width 25 -lines 20 -padding 25 -hide-scrollbar -show-icons -drun-icon-theme
+rofi -modi run,drun -show drun -location 7 -xoffset 14 -yoffset -47 -line-padding 4 -columns 1 -width 25 -lines 20 -padding 25 -hide-scrollbar -show-icons -drun-icon-theme
--- polybar/script/vpn-anyconnect.sh
+++ polybar/script/vpn-anyconnect.sh
@@ -5,5 +5,5 @@ IFACE=$(ifconfig | grep tun | awk '{print $1}')
 if [ "$IFACE" = "tun0:" ]; then
     echo "  : $(ifconfig tun0 | grep inet | awk '{print $2}' | cut -f2 -d ':')%{u-}"
 else
-    echo ""
+    echo "" 
 fi

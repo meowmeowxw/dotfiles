09d761d53e92f5aa58a37c7de6cd881d3cc2e678
meowmeowxw <giovanni.disanti@protonmail.com>
2020-01-03 15:43:26 +0100

    create directory nvim installer

--- larch/install.sh
+++ larch/install.sh
@@ -16,6 +16,7 @@ nvim()
 	echo ${START} "nvim"
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	mkdir -p ${CONFIG_DIR}/nvim
 	cp nvim/init.vim ${CONFIG_DIR}/nvim/init.vim
 	cp nvim/colors/* ${CONFIG_DIR}/nvim/colors/
 	echo ${END} "nvim"

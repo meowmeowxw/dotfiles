51ba92ff4e85333f2ce3610d8a03574e59896cd7
meowmeowxw <giovanni.disanti@protonmail.com>
2021-08-31 10:49:18 +0200

    Fix config fish FZF

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,9 +7,7 @@ set -x MANPAGER "nvim -u NORC --noplugin -c 'set ft=man' -"
 set -x MOZ_X11_EGL 1
 set -x GOPATH ~/.local/go
 set -x EDITOR nvim
-set -x FZF_DEFAULT_OPTS "
-  --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
-  --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
+set -x FZF_DEFAULT_OPTS "--color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54"
 set fish_greeting
 
 source /etc/grc.fish
